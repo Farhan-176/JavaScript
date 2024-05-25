5e13436fd42ea4748a5e48c3cf3bd2dc9524ea61
Farhan Afridi <156736181+Farhan-176@users.noreply.github.com>
2024-05-26 03:02:49 +0500

    ALERTS, VARIABLES THINGS.
    ALERTS, VARIABLES FOR (STRINGS AND NUMBERS), VARIABLE NAME

---
+++ work 01/alerts...js
@@ -0,0 +1,32 @@
+
+//   chap 1
+// start of javascript by alert
+
+
+//  alert("Thanks for your visiting!");
+
+
+
+
+// to open console 
+// right click mouse and click on isnpect then click console shown in tabs
+
+
+
+// // now variable
+// var name = "FARHAN";
+// console.log(name);
+
+
+// // js add only number without qoution mark
+
+
+// var weight = 150;
+// console.log(weight + 24);
+
+// // a number enclosed by quotation marks is not a number, but a string, and JavaScript can't do addition on it.
+
+// var number = 150;
+// console.log(number);
+
+// // js cant print the name that begin with the number or the name is number 
