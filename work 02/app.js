328b16bc9a73450c5a4d8d20a1fc2b7bdeb3ff13
Farhan Afridi <156736181+Farhan-176@users.noreply.github.com>
2024-05-28 14:25:55 +0500

    MATH EXPRESSION ,CONCATING, PROMPT
    MATH EXPRESSION(FAMILIAR OPERATOR,ELIMINATING AMBIGUITY), CONCATING TEXT STRINGS, PROMPT.

---
+++ work 02/app.js
@@ -0,0 +1,137 @@
+// // math expression chap 5
+
+// var number2 = 2 + 2;
+// var number3 = 2 * 5;
+// var number4 = 2 - 5;
+// var number5 = 14 % 3;
+// var number6 = number + number;
+// console.log(number2 , number3 , number4, number5 , number6);
+// alert(number2);
+
+
+
+// let num1 = 3461327;
+// console.log(num1 % 5);
+
+
+
+
+
+// // increment and decrement can e done by putting it after the variable
+
+
+// var num = 10;
+// var newNum = num++ ;
+// console.log(newNum);
+
+
+
+// let workOutDay = 1;
+
+// console.log(workOutDay++); //1
+// console.log(++workOutDay); // 3
+// console.log(++workOutDay); //4
+
+
+// let bullets = 11;
+// console.log(bullets--);//11
+// console.log(--bullets);//9
+
+
+
+
+// // y precedence rules. As in algebra, the rule that applies here is that multiplication operations are completed before addition operations.
+// // in the following it follow the above rule
+// var total = 1 + 4 * 4;
+// console.log(total);
+
+
+
+// // if we want to add first before the multiplication then
+// var total1 = (1 + 4) * 4;
+// console.log(total1);
+
+
+
+// // or
+// var total = 1 + (4 * 4);
+// console.log(total);
+
+
+
+
+
+
+// // concatenates
+
+
+
+// let table = +prompt('enter table name'); //2
+// let count = 1;
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+
+
+
+// alert("Thanks," + "farhan" + "!");
+// console.log(alert);
+
+
+
+
+
+
+
+
+// // prompt chap 9 asking user for input
+
+
+
+
+
+// var numberOfCats = prompt("How many cats?");
+// console.log(numberOfCats);
+
+// let userName = prompt("what is your name?");
+// console.log("Welcome " + userName);
+
+
+
+
+// let num1 = +prompt("enter a number");
+// let num2 = +prompt("enter another number");
+// console.log(num1 + num2);
+
+
+
+// let userName = prompt("What is your name?");
+// let num1 = +prompt('enter a number');
+// let num2 = +prompt('enter another number');
+
+// document.write("Welcome " + userName + "." + "<br />Your total is: " + (num1 + num2));]
+
+
+
+
+
+
+
