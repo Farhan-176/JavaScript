631ae9e170730cc3ca9e7aef533eed9bbf72a79c
Farhan Afridi <156736181+Farhan-176@users.noreply.github.com>
2024-06-05 12:09:18 +0500

    Variable for strings Solution
    variable for strings task solution.

---
+++ TASKS Solution/variable for strings/app.js
@@ -0,0 +1,62 @@
+// Question  no 1
+// let username;
+
+
+
+// question no 2
+// var myname = "Farhan Afridi";
+
+
+// question no 3
+// var message = "HELLO WORLD";
+// alert(message);
+
+// question no 4
+
+// var name = "Ali khan";
+// var age = "17 year old";
+// the checkbox is not available in modern windows
+// var subject = "SSUET";
+// var developmenttopic = "Mobile & Cloud Computing - Module A"
+// alert(name);
+// alert(age);
+// alert(subject);
+// alert(developmenttopic);
+
+// QUESTION NO 5
+// var pizzaword = "PIZZA \n PIZZ \n PIZ \n PI \n P "
+// alert(pizzaword);
+
+// QUESTION NO 6
+// var jobtitle = "Software developer";
+// var geographicallocation = "USA";
+// var partnername = "ABC";
+// var numberofchild = "2 kids";
+
+// var fortune = "You will be a " + jobtitle + " in " + geographicallocation + ", and married to " + partnername + " with " + numberofchild;
+// alert(fortune);
+
+// QUESTION NO 7
+// var text = "my email address is "
+// var email = "abc@gmail.com"
+// var fullemail = text.concat(email);
+// alert(fullemail);
+
+
+// QUESTION NO 8
+// var book = " 'A smarter way to learn javascript' ";
+// var txt = "i am tryig to learn from the book ";
+// var fullbook = txt.concat(book);
+// alert(fullbook);
+
+
+var fullname = "Ali khan";
+var theage = "17 year old";
+var thesubject = "SSUET";
+var thedevelopmentcourse = "Mobile & Cloud Computing - Module A"
+
+var fullmsg = "My name is " + fullname + " i am " + theage + " I study in " + thesubject + " i take " + thedevelopmentcourse + " classes." ;
+
+alert(fullmsg);
+
+
