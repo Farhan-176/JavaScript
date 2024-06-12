eec97929433419826b2cce1b06caa7ff24203282
Farhan Afridi <156736181+Farhan-176@users.noreply.github.com>
2024-06-12 20:23:54 +0500

    Variable for Numbers Task Solution

---
+++ TASKS Solution/variable for numbers/app.js
@@ -0,0 +1,21 @@
+// question no 1
+var age = 14;
+alert("I am " + age + " year old");
+
+
+// question no 2
+// var askuser = prompt("how many time you visit our website?")
+// var telluser = "you have visited our webiste " + askuser + " times";
+// document.write(telluser);
+
+// question no 3
+// var birthdate = prompt("what is your date of birth?")
+// var userbirthdate = "My birth year is " + birthdate + ". " ;
+// document.write(userbirthdate);
+
+// question no 4
+// var visitorname = prompt("What is your name?")
+// var productname = prompt("What is product name?")
+// var quantity = prompt("How many products you want to order")
+// var orderdetail = visitorname + " ordered " + quantity + " " + productname + "(s) on XYZ Clothing Stores."
+// document.write(orderdetail);
