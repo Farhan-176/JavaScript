cef667bdf7394d7bd84b6f23db0b1f025c86c426
Farhan Afridi <156736181+Farhan-176@users.noreply.github.com>
2024-06-22 16:25:18 +0500

    Variable Names Task Solution
    Variable Names Tasks Questions  Solution Added.

---
+++ TASKS Solution/variable Names legal and illegal/app.js
@@ -0,0 +1,25 @@
+// question no 1
+
+// var firstname , middlename , lastname ;
+
+
+// question no 2
+// legal name:
+
+// var first1name;
+// var middlename$;
+// var last_name;
+// var cast_;
+// var full_address;
+
+// // illegal name:
+// var -firstname;
+// var 2middlename;
+// var last%name;
+// var cast)-;
+// var fulladdress#;
+
+
+
+// question no 3
+// answer is in html portion.
