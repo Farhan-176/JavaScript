992864425fbb2b591b7d624fa6e3df762392555b
Farhan Afridi <156736181+Farhan-176@users.noreply.github.com>
2024-06-22 16:21:53 +0500

    Math Expression Task Solution
    Math Expression Task Questions  Solutions.

---
+++ TASKS Solution/Math Expression/math.js
@@ -0,0 +1,439 @@
+// // question no 1
+// var number1 = 9;
+// var number2 = 13;
+// var addnumbers = number1 + number2;
+// var showaddition = "the sum of " + number1 + " and " + number2 + " is "  + addnumbers;
+// alert(showaddition);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // question no 2
+// var subnumbers = number2 - number1;
+// var showsubtraction = "the subtract of " + number2 + " and " + number1 + " is "  + subnumbers;
+// alert(showsubtraction);
+
+
+// var mutiplicationnumbers = number2 * number1;
+// var showmultiplication = "the multiplication of " + number1 + " and " + number2 + " is "  + mutiplicationnumbers;
+// alert(showmultiplication);
+
+
+// var divisionnumbers = number2 / number1;
+// var showdivision = "the division of " + number2 + " and " + number1 + " is "  + subnumbers;
+// alert(showdivision);
+
+
+// var modulenumbers = number2 % number1;
+// var showmodule = "the module of " + number2 + " and " + number1 + " is "  + modulenumbers;
+// alert(showmodule);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // question mo 3
+// let variable;
+// document.write("Variable after variable decleration is : " + variable + "<br / >");
+
+// let variableshow = 3;
+// document.write("Inital value is : " + variableshow + "<br / >");
+
+// let variableincrement = 3;
+// let newvariableincrement = ++variableincrement ;
+// document.write("the value after increment is : " + newvariableincrement + "<br / >");
+
+
+// let value = 3;
+// let valueadditon = (3 + 8);
+// document.write("Value After addition : " + valueadditon + "<br / >");
+
+// let valueTodecrement = 11;
+// let valueafterdecrement = --valueTodecrement;
+// document.write("Value after decrement : " + valueafterdecrement + "<br / >");
+
+// let valueremainder = (11 % 10);
+// document.write("the remainder is : " + valueremainder);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question no 4
+// let oneticketprice = 600 ;
+// let askuserabouttickets = prompt("How many tickets you want to buy?");
+// let calculatetickets = (oneticketprice * askuserabouttickets);
+
+// document.write("Total cost to buy  " + askuserabouttickets + " tickets to a movie is " + calculatetickets + " PKR")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question no 5
+// let table = +prompt('enter table name'); //4
+// let count = 1;
+
+
+// document.write("Multiplication table of " + table + " is " + "<br />" );
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+// document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ******************************CONVERTER************************
+// question no  6
+// let CelsiusTemp = prompt("What is Temprature today?");
+// let Celsiustofahrenheit = (CelsiusTemp * 9 / 5) + 32;
+// document.write(CelsiusTemp + " `C " + " is " + Celsiustofahrenheit + " `F" + "<br />");
+
+// let FahrenheitTemp = prompt("What is Temprature today?");
+// let FahrenheittoCelsius = (FahrenheitTemp - 32) * 5 / 9 ;
+// document.write(FahrenheitTemp + " `F " + " is " + FahrenheittoCelsius + " `C");
+
+
+
+
+
+
+// question no 7 
+// document.write("<H1>SHOPPING CART<H1/>");
+
+
+
+// var askprice =+ prompt("What is the price of item 1?");
+// var putprice = document.write("Price of item 1 is " + askprice + "<br />");
+
+// var askquantity = prompt("What is quantity of item 1 ?");
+// var putquantity = document.write("Quantity of item 1 is " + askquantity + "<br />");
+
+
+// var askprice1 =+ prompt("What is the price of item 2 ?");
+// var putprice1 = document.write("Price of item 2 is " + askprice1 + "<br />");
+
+
+// var askquantity1 = prompt("What is quantity of item 2 ?")
+// var putquantity1 = document.write("Quantity of item 2 is " + askquantity1 + "<br />" + "<br />");
+
+
+// var shipingcharges = 150;
+// document.write(" Shipping charges "+ shipingcharges + "<br />" + "<br />");
+
+// var totalcharges = (askprice * askquantity + askprice1 * askquantity1  + shipingcharges);
+
+
+// var totalcost = document.write(" Total Cost of Your Order is " + totalcharges + " PKR ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question no 7 
+
+
+
+// var askstudenttotal =+ prompt("What is Total Marks?");
+// document.write("Total Marks : " + askstudenttotal + "<br />")
+
+// var askstudentobtain = prompt("What is obtain Marks?");
+// document.write("ObtainMarks : " + askstudentobtain + "<br />")
+
+// var percentage = (askstudentobtain * 100 ) / askstudenttotal;
+// document.write("Percentage : " + percentage + "%"  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question no 8 
+
+// var askperson =+ prompt("How many US Dollors do you have ?");
+// var asksameperson =+ prompt("How many Saudi Riyals do you have ?");
+
+// var exchangerate = (askperson * 280) + (asksameperson * 78);
+
+
+
+// document.write("<H1>Currency in PKR<H1/>");
+// document.write("Total Currency in PKR : " + exchangerate);
+
+
+
+
+
+
+
+
+
+
+
+
+// Question no 10 
+
+
+// var asknum =+ prompt("GIVE ANY NUMBER!");
+// var giveoutput = (asknum + 5 * 10 / 2);
+// alert(giveoutput);
+
+
+
+
+
+
+
+
+// question no 11
+// document.write("<H1>The Age Calculator<H1/>");
+
+// var currentyear = 2024;
+// document.write("Current year : " + currentyear + "<br />");
+
+// var askuseryear =+ prompt("What is your Birthday Year?")
+// document.write("Birth Year : " + askuseryear + "<br />");
+
+// var userbirth = currentyear - askuseryear;
+// var userchancebirth = userbirth - 1;
+// document.write("They are either " + userchancebirth + " or " + userbirth + " year old" + "<br />") 
+
+
+
+
+
+
+
+
+
+
+// question no 12
+
+
+// document.write("<H1>The Geometrizer<H1/>");
+// var askradius =+ prompt("What is the radius of Circle ?");
+// document.write("The Radius of Circle: " + askradius  + "<br />");
+
+
+
+// var pievalue = 3.142;
+// var CalculateCircumfernece = 2 * pievalue * askradius;
+// document.write("The Circumfernece is : " + CalculateCircumfernece  + "<br />");
+
+
+
+// var areaofcircle = pievalue *(askradius*askradius);
+// document.write("The Area is : " + areaofcircle  + "<br />");
+
+
+
+
+
+
+
+
+
+
+
+// question no 13
+
+
+
+// var askvalue = prompt("What is the Value of variable 'a' ?");
+
+// document.write("Result: "  + "<br />" + "The Value of a is : " + askvalue  + "<br />" + "---------------------------"  + "<br />" + "<br />");
+
+
+// var incrementvalue = ++askvalue;
+// document.write("The value of ++a is: " + incrementvalue + "<br />" );
+// document.write("Now the value of a is: " + incrementvalue + "<br />" + "<br />" );
+
+
+
+// var incrementvalueaftervariable = ++askvalue;
+// document.write("The value of a++ is: " + incrementvalue++ + "<br />" );
+// document.write("Now the value of a is: " + incrementvalueaftervariable + "<br />" + "<br />" );
+
+
+
+
+// var decrmentvalue = --askvalue;
+// document.write("The value of --a is: " + decrmentvalue +"<br />")
+// document.write("Now The value of a is: " + decrmentvalue +"<br />"+"<br />")
+
+
+
+
+// var decrementvalueaftervariable = --askvalue;
+// document.write("The value of a-- is: " + decrmentvalue-- +"<br />")
+// document.write("Now the value of a is: " + decrementvalueaftervariable)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// QUESTION NO 14
+// var a = 2;
+// document.write("a is: " + a + "<br />")
+// var b = 1;
+// document.write("b is: " + b + "<br />")
+
+// var result = (--a + --b) + (++b + b);
+// document.write("RESULT is: " + result + "<br />")
+
+
+
+
+
+
+
+
+
+
+
+
+
+//QUESTION NO 16
+
+// var askSnacks = prompt("What is your Favourite Snacks Name?");
+// var Snacksname = askSnacks;
+// document.write("Favourite Snacks: " + Snacksname + "<br />");
+
+
+// var askAge = prompt("What is your Age?");
+// var CurrentAge = askAge;
+// document.write("Current Age: " + CurrentAge + "<br />")
+
+// MaximumAge = 85;
+// document.write("Estimate Maximum Age is: " + MaximumAge + "<br />");
+
+// var askamount = prompt("What is amount of Snacks Per Day?");
+// document.write("Amounts of Snacks Per Day: " + askamount + "<br />");
+
+
+// var calcualteSnacks = (MaximumAge-CurrentAge)*askamount;
+// document.write("You will need  " + calcualteSnacks + " " +  Snacksname + " to last you untill the rip old age of 85") ;
