4f04e80ff5a2b9ec99dee2bd2fdd393c2470f175
Farhan Afridi <156736181+Farhan-176@users.noreply.github.com>
2024-08-04 15:55:47 +0500

    Switch, While , Do While loops
    Switch, While , Do While loops in detail

---
+++ work 10/apps.js
@@ -0,0 +1,119 @@
+// chap 39 AND cahp 40
+
+
+
+// simple example for understandig the switch statement as it work same as if statement but it differ in syntax 
+
+
+
+// let day = new Date().getDay();
+// let dayName;
+
+// if (day === 0) {
+//     dayName = "sunday";
+// }
+// else if (day === 1) {
+//     dayName = "monday";
+// }
+// else if (day === 2) {
+//     dayName = "tuesday"
+// }
+// else if (day === 3) {
+//     dayName = "wednesday";
+// }
+// else {
+//     dayName = "unkown to me";
+// }
+// console.log(dayName)
+
+
+// switch statement
+// let day = new Date().getDay();
+// let dayName;
+
+// switch (day) {
+//     case 0:
+//         dayName = "sunday";
+//         console.log(dayName);
+//         break;    
+//     case 1:
+//         dayName = "monday";
+//         console.log(dayName);
+//         break;
+//     case 2:
+//         dayName = "tuesday";
+//         console.log(dayName);
+//         break;  
+//     case 3:
+//         dayName = "wedneday";
+//         console.log(dayName);
+//         break;  
+//     default:
+//         dayName = "saturday";
+//         console.log(dayName);
+//         break;
+// }
+
+
+// example of switch
+
+// let day = prompt("enter a day name?");
+
+// switch (day) {
+//     case "Monday":
+//         console.log("office");
+//         break;
+//     case "Tuesday":
+//         console.log("doffice");
+//         break;
+//     default:
+//          console.log("holiday");
+//         break;
+// }
+
+
+
+// chap 41
+
+// var i = 0;
+// while (i  <= 3 ) {
+//     alert(i);
+//     i++;
+// }
+
+
+// chap 42
+
+// var i = 0;
+// do {
+//     alert(i);
+//     i++;
+// }
+// while (i  <= 3 )
+
+
+
+
+// chap 43
+// in html
+
+
+// cha 44 commenting 
+
+
+//  types of Comment
+
+
+// 
+/* */
+
+/*var i = 0; */
+ 
+// do {
+//     alert(i);
+//     i++;
+// }
+// while (i  <= 3 )
+
+
+
