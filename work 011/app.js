3dfcfbf532da2989b37a2548afc793ec94ff5b26
Farhan Afridi <156736181+Farhan-176@users.noreply.github.com>
2024-08-05 20:12:58 +0500

    Events Topic
    Events Topics in Detailed.

---
+++ work 011/app.js
@@ -0,0 +1,91 @@
+
+// chap 45 events:links
+
+
+
+// All of these user actions—clicking a button, moving the mouse, moving from one field to
+// another in a form, selecting a different option—are known as events. JavaScript code that
+// responds to an event—for example, displaying a guarantee or swapping an image when the
+// pointer hovers over it—is called an event handler.
+// in this chapter we will learn INLINE EVENT HANDLING
+
+
+// The message inside the parens is surrounded by single quotes, not the double quotes because there is no
+// double quotes in double quotes and no single quote in single quote
+
+// function showmsg0(){
+//     alert("hello i am js instructor!!!!!!");
+// }
+
+
+// function showmsg1(){
+//     alert("this is with Javascript:void(0)");
+// }
+
+// function showmsg2(message){
+//     alert(message);
+// }
+
+
+
+
+
+// chap 46
+
+// function showpicmessgae(){
+//      alert("this alert by function");
+// }
+
+
+// At this point don't worry now about how the function expand grows the paragraph. You'll learn about that later.
+
+
+// let v1 = document.querySelectorAll("#paraexpand");
+// function expand(){
+//     let v2 = paraexpand[0].value;
+//     let v3 = v2 + "2344" ;
+//     console.log(v3)
+// }
+
+
+
+// chap 47  in html
+// chap 48  in html
+
+
+
+
+// chap 49
+// function checkAddress(fieldId) {
+//     if (document.getElementById(fieldId).value === "") {
+//         alert("Email address required.");
+//     }
+// }
+
+
+// chap 50
+// function fillCity() {
+//     var cityName;
+//     var zipEntered = document.getElementById("zip").value;
+//     switch (zipEntered) {
+//         case "60608":
+//             cityName = "Chicago";
+//             break;
+//         case "68114":
+//             cityName = "Omaha";
+//             break;
+//         case "53212":
+//             cityName = "Milwaukee";
+//     }
+//     document.getElementById("city").value = cityName;
+// }
+
+
+
+
+// chap 51
+// function expandLoris() {
+//      var expandedParagraph = "Slow lorises are a group of several species of trepsirrhine primates which make up the genus Nycticebus.They have a round head, narrow snout, large eyes, and a variety ofdistinctive coloration patterns that are species - dependent.The hands and feet of slow lorises have several adaptations that give them a pincer - like grip and enable them to grasp branches for long periods of time. Slow lorises have a toxic bite, a rare trait among mammals.";
+//      document.getElementById("slowLoris").innerHTML = expandedParagraph;
+
+// }
