931aeabf23bb8fffb821126af4544e820bb9486f
Farhan Afridi <156736181+Farhan-176@users.noreply.github.com>
2024-08-11 21:10:42 +0500

    Manipulation and swapping images
    Manipulation and swapping images.

---
+++ work012/app.js
@@ -0,0 +1,24 @@
+// function makeafter() {
+//     document.getElementById("before").className = "hidden";
+// }
+
+
+
+
+
+
+
+
+
+
+
+// function swappic() {
+//     let pic1 = document.getElementById("before");
+//     pic1.src = "images/images.webp";
+// }
+
+
+// function swappic(eid,newpic) {
+//     let pic1 = document.getElementById(eid);
+//     pic1.src = newpic;
+// }
